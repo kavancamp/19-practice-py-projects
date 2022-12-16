8df93a28abb3e902c89408c3b800961015e45844
Keenah <kvancamp@mail.com>
2022-12-15 18:26:22 -0600

    Create replace_word.py

---
+++ replace_word.py
@@ -0,0 +1,7 @@
+def replace_word():
+  str = "Hello, I am Keenah! lorem ipsum blah blah blah"
+  word_to_replace = input("Enter the word to replace: ")
+  word_replacement = input("Enter the word to replace: ")
+  print(str.replace(word_to_replace, word_replacement))
+
+replace_word()
