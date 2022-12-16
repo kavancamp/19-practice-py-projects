1691ea752d596ff2bcdcbea77062b1c205e92051
Keenah <kvancamp@mail.com>
2022-12-15 19:48:31 -0600

    added spacing

--- basic_calculator.py
+++ basic_calculator.py
@@ -6,19 +6,19 @@
 
 def add(a, b):
   answer = a + b
-  print(str(a) + " + " + str(b) + " = " + str(answer))
+  print(str(a) + " + " + str(b) + " = " + str(answer) + "\n")
   
 def sub(a, b):
   answer = a - b
-  print(str(a) + " - " + str(b) + " = " + str(answer))
+  print(str(a) + " - " + str(b) + " = " + str(answer) + "\n")
 
 def multiply(a, b):
   answer = a * b
-  print(str(a) + " * " + str(b) + " = " + str(answer))
+  print(str(a) + " * " + str(b) + " = " + str(answer) + "\n")
 
 def div(a, b):
   answer = a / b
-  print(str(a) + " / " + str(b) + " = " + str(answer))
+  print(str(a) + " / " + str(b) + " = " + str(answer) + "\n")
 
 while True:
 
