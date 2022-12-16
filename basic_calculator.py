5257fe99a109ae0ff6846f542981a20c5641114a
Keenah <kvancamp@mail.com>
2022-12-15 19:47:13 -0600

    added while loop

--- basic_calculator.py
+++ basic_calculator.py
@@ -20,36 +20,37 @@ def div(a, b):
   answer = a / b
   print(str(a) + " / " + str(b) + " = " + str(answer))
 
+while True:
 
-print("A. Addition")
-print("B. Subtraction")
-print("C. Multiplication")
-print("D. Division")
-print("E. Exit")
+  print("A. Addition")
+  print("B. Subtraction")
+  print("C. Multiplication")
+  print("D. Division")
+  print("E. Exit")
 
-choice = input("Enter your choice: ")
+  choice = input("Enter your choice: ")
 
-if choice == "a" or choice == "A":
-  print("Addition")
-  a = int(input("input first number: "))
-  b = int(input("input second number: "))
-  add(a, b)
-elif choice == "b" or choice == "B":
-  print("Subtraction")
-  a = int(input("input first number: "))
-  b = int(input("input second number: "))
-  sub(a, b)
-elif choice == "c" or choice == "C":
-  print("Multiplication")
-  a = int(input("input first number: "))
-  b = int(input("input second number: "))
-  multiply(a, b)
-elif choice == "d" or choice == "D":
-  print("Division")
-  a = int(input("input first number: "))
-  b = int(input("input second number: "))
-  div(a, b)
-elif choice == "e" or choice == "E":
-  print("Have a nice day!")
-  quit()
+  if choice == "a" or choice == "A":
+    print("Addition")
+    a = int(input("input first number: "))
+    b = int(input("input second number: "))
+    add(a, b)
+  elif choice == "b" or choice == "B":
+    print("Subtraction")
+    a = int(input("input first number: "))
+    b = int(input("input second number: "))
+    sub(a, b)
+  elif choice == "c" or choice == "C":
+    print("Multiplication")
+    a = int(input("input first number: "))
+    b = int(input("input second number: "))
+    multiply(a, b)
+  elif choice == "d" or choice == "D":
+    print("Division")
+    a = int(input("input first number: "))
+    b = int(input("input second number: "))
+    div(a, b)
+  elif choice == "e" or choice == "E":
+    print("Have a nice day!")
+    quit()
 
