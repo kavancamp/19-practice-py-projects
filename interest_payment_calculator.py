ae5471e3395652c5345a0d2e4e1039a9d924fd40
Keenah <kvancamp@mail.com>
2022-12-17 15:32:10 -0600

    Update interest_payment_calculator.py

--- interest_payment_calculator.py
+++ interest_payment_calculator.py
@@ -2,20 +2,18 @@
 #calculate monthly payment 
 #print
 
-
 def main():
-  print("This is monthly payment loan calculator")
+  print(" This is a monthly payment loan calculator ")
   print("")
-
-  principal = float(input("input the loan amount: ")) 
+  
+  principal = float(input("Input the loan amount: "))
   apr = float(input("Input the annual interest rate: "))
-  years = int("Input amount of years: ")
+  years = int(input("Input amount of years: "))
   
   monthly_interest_rate = apr / 1200
-  number_of_months = years * 12 
-
-  monthly_payment = principal * monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-number_of_months))
-
-  print("The monthly payment for this load is: " + str(monthly_payment))
+  amount_of_months = years * 12
+  monthly_payment = principal * monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-amount_of_months))
+  
+  print(" The monthly payment for this loan is: %.2f " % monthly_payment)
 
 main()
