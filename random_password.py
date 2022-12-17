c417b8e816a406bc69ab428de7029fb9c64bb690
Keenah <kvancamp@mail.com>
2022-12-17 16:46:14 -0600

    Update random_password.py

--- random_password.py
+++ random_password.py
@@ -3,3 +3,28 @@
 #generate password and print  
 #exit if answer is no 
 
+import string 
+import random 
+
+characters = (string.ascii_letters + string.digits + "!@#$%^&*")
+
+def generate_password():
+  password_length = int(input("Enter password length: "))
+
+  random.shuffle(characters)
+  password = [] 
+
+  for x in range(password_length):
+    password.append(random.choice(characters))
+
+  random.shuffle(password)
+
+  password = "".join(password)
+  print(password)
+  
+option = input(" Do you want to generate a password? (Yes/No")
+if option == "Yes":
+  generate_password()
+elif option == "No":
+  print("Program ended")
+  quit()
